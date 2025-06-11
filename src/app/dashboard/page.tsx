b6774a39daacab35488e9348e6bd82723094d3c7
junior luiz <juninhoc4p@gmail.com>
2025-06-11 09:24:56 -0300

    Correção em authOptions

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../lib/authOptions";
 import { Metadata } from "next";
 import { appName } from "@/config";
 
